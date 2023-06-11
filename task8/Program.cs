27215b34bfbbb873abb35d8a069dc6884d38efe5
MJ <javohirbek6189@gmail.com>
2023-06-11 18:55:31 +0300

    Task 8 Copm

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число N");
+string? numberN = Console.ReadLine();
+int stringNumN = Convert.ToInt32(numberN);
+
+for (int i = 0; i<=stringNumN; i++)
+{
+    if (i%2==0)
+    {
+        Console.WriteLine(i);
+    }
+}
