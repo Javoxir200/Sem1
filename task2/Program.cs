ec8818f5a324564eddb8692f1e07567c557cb6be
MJ <javohirbek6189@gmail.com>
2023-06-11 17:29:08 +0300

    Home Work

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите первое числа");
+string? numA = Console.ReadLine();
+int stringnumA = Convert.ToInt32(numA);
+
+Console.WriteLine("Введите второе числа");
+string? numB = Console.ReadLine();
+int stringnumB = Convert.ToInt32(numB);
+
+int max, min;
+
+max=stringnumA;
+
+if (stringnumB>stringnumA)
+{
+    max=stringnumB;
+    min=stringnumA;
+}
+else
+{
+    max =stringnumA;
+    min =stringnumB;
+}
+
+Console.Write("Мах: ");
+Console.WriteLine(max);
+Console.Write("Мин: ");
+Console.WriteLine(min);
---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число: A");
+string? numA = Console.ReadLine();
+int stringnumA = Convert.ToInt32(numA);
+//Console.WriteLine(numberInt * numberInt);
+
+Console.WriteLine("Введите число: B");
+string? numB = Console.ReadLine();
+int stringnumB = Convert.ToInt32(numB);
+
+Console.WriteLine("Введите число: C");
+string? numC = Console.ReadLine();
+int stringnumC = Convert.ToInt32(numC);
+int max;
+
+max = stringnumA;
+if (stringnumB > stringnumA)
+    max = stringnumB;
+if (stringnumC > max)
+    max = stringnumC;
+
+Console.Write("Максималное число: ");
+Console.WriteLine(max);
+
+
+
