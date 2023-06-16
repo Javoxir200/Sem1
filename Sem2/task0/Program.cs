8a6fab65a3cc6958f32e0f0aab884c0b791af444
MJ <javohirbek6189@gmail.com>
2023-06-16 13:45:23 +0300

    Home work Seminar 2 version2

--- Sem2/task0/Program.cs
+++ Sem2/task0/Program.cs
@@ -1,8 +1,18 @@
-﻿int number = Random.Shared.Next(100, 1000);
+﻿int number = Random.Shared.Next(10, 100);
 Console.WriteLine(number);
 
-int lastDigit = number / 100;
-int firstDigit = number %10;
+int lastDigit = number / 10;
+int firstDigit = number % 10;
 
-Console.Write(lastDigit);
-Console.Write(firstDigit);
+if (firstDigit>lastDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else if(lastDigit>firstDigit)
+{
+    Console.WriteLine(lastDigit);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
---
+++ Sem2/task11/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = Random.Shared.Next(100, 1000);
+Console.WriteLine(number);
+
+int firstDigit = number / 100;
+int lastDigit = number % 10;
+
+int result = firstDigit * 10 + lastDigit; 
+
+Console.WriteLine(result);
+
